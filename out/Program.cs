3fb832b7dd3d720aebf06d77a1ba1e05e992224d
Rajesh Pareek <rajesh.pareek@intimetec.com>
2026-04-07 10:26:32 +0530

    Comming stuff

---
+++ out/Program.cs
@@ -0,0 +1,14 @@
+﻿var list = new List<int> { 1, 2, 3 };
+
+var query = list.Where(x => x > 1);
+var materialized = list.Where(x => x > 1).ToList();
+
+list.Add(10);
+
+foreach (var x in query)
+    Console.Write(x + " ");
+
+Console.Write("| ");
+
+foreach (var x in materialized)
+    Console.Write(x + " ");
